7eafcd9ed8b597b83ee1cd458e0d8fd06e0b2084
JiaJiaLine <1771171610@qq.com>
2026-06-16 15:14:02 +0800

    Configure backend war deployment

--- backend/src/main/java/com/galgallery/GalGalleryApplication.java
+++ backend/src/main/java/com/galgallery/GalGalleryApplication.java
@@ -3,13 +3,19 @@ package com.galgallery;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @MapperScan("com.galgallery.mapper")
 @SpringBootApplication
-public class GalGalleryApplication {
+public class GalGalleryApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(GalGalleryApplication.class, args);
     }
-}
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GalGalleryApplication.class);
+    }
+}
